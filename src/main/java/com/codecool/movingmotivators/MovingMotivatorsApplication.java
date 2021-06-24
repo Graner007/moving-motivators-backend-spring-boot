ba6e869f6324278e9664b8c1d3520769e0b1f228
Graner007 <graner.bence007@gmail.com>
2021-06-24 10:36:25 +0200

    init project

---
+++ src/main/java/com/codecool/movingmotivators/MovingMotivatorsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.movingmotivators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingMotivatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovingMotivatorsApplication.class, args);
+    }
+
+}
