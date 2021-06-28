9961e1cfa1ae47432b70352891b55235e45becb4
bodisB <bencebodis07@gmail.com>
2021-06-28 16:10:35 +0200

    fix: delete board model

--- src/main/java/com/codecool/movingmotivators/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.movingmotivators.repository;
-
-import com.codecool.movingmotivators.model.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
