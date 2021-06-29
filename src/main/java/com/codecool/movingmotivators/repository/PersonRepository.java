0ee8cd8ee73fc1f92353efeca0ac864f9d8c97e0
Graner007 <graner.bence007@gmail.com>
2021-06-29 14:30:10 +0200

    add registration

--- src/main/java/com/codecool/movingmotivators/repository/PersonRepository.java
+++ src/main/java/com/codecool/movingmotivators/repository/PersonRepository.java
@@ -4,6 +4,11 @@ import com.codecool.movingmotivators.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByUsername(String username);
+
 }
