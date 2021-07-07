14544a660c62735fde44f9ca6abd61b14c565281
kisstamas86 <kisstamas861023@gmail.com>
2021-07-07 21:41:11 +0200

    serve static files from jar, remove copy hack from dockerfile

--- src/main/java/com/codecool/movingmotivators/service/MediaService.java
+++ src/main/java/com/codecool/movingmotivators/service/MediaService.java
@@ -2,23 +2,16 @@ package com.codecool.movingmotivators.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 
 @Service
 public class MediaService {
 
-    private static final String MEDIA_PATH = "./src/main/resources/media/";
+    private static final String MEDIA_PATH = "../../../../media/";
 
     public byte[] getCardImageByName(String imageName) {
         try {
-            InputStream in = this.getClass().getClassLoader().getResourceAsStream("classes/media/" + imageName);
-            System.out.println(in);
-            return in != null ? new byte[in] : new byte[0];
-//            return Files.readAllBytes(Paths.get(MEDIA_PATH + imageName));
+            return getClass().getResourceAsStream(MEDIA_PATH + imageName).readAllBytes();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
